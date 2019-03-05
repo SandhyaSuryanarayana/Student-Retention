1f24675a947c0f91d92d42a2ffb637d23d22ea6f
Sandhya Suryanarayana <ssandhya.2211@gmail.com>
2019-03-05 20:26:56 +0000

    Merged static and progress for fall 2011. Merged Financial and dropout lablel

--- StudentRetention.R
+++ StudentRetention.R
@@ -5,20 +5,6 @@ sum(is.na(Fall_2011_ST$Address1))
 summary(Fall_2011_ST)
 str(Fall_2011_ST)
 
-#Fall 2011 progress data
-nrow(Fall_2011_SP)
-sum(is.na(Fall_2011_SP))
-summary(Fall_2011_SP)
-str(Fall_2011_SP)
-
-#Financial Aid table
-View(Financial_Aid)
-nrow(Financial_Aid) #13769
-sum(is.na(Financial_Aid)) #305867
-str(Financial_Aid)
-View(Financial_Aid_1)
-
-
 Fall_2011_ST$StudentID<- as.numeric(Fall_2011_ST$StudentID)
 
 Fall_2011_ST$RegistrationDate<-as.Date(as.character(Fall_2011_ST$RegistrationDate),"%Y%m%d")
@@ -60,7 +46,11 @@ Fall_2011_ST$GatewayMathStatus <-as.factor(Fall_2011_ST$GatewayMathStatus)
 
 Fall_2011_ST$GatewayEnglishStatus<-as.factor(Fall_2011_ST$GatewayEnglishStatus)
 
-##Student progress data
+#Fall 2011 progress data
+nrow(Fall_2011_SP)
+sum(is.na(Fall_2011_SP))
+summary(Fall_2011_SP)
+str(Fall_2011_SP)
 
 str(Fall_2011_SP)
 
@@ -81,67 +71,76 @@ Fall_2011_SP$DegreeTypeSought <- as.factor(Fall_2011_SP$DegreeTypeSought)
 summary(Fall_2011_SP)
 nrow(Fall_2011_SP)
 
-summary(Fall_2011_ST)
-nrow(Fall_2011_ST)
 
-sum(is.na(Fall_2011_ST$GatewayMathStatus))
+#Financial Aid table
+View(Financial_Aid)
+nrow(Financial_Aid) #13769
+colSums(is.na(Financial_Aid)) #305867
+str(Financial_Aid)
+summary(Financial_Aid)
+View(Financial_Aid_1)
 
-st_sp <- merge(Fall_2011_ST,Fall_2011_SP,by="StudentID")
-
-View(st_sp)
-
-nrow(st_sp) ##1759
-
-a<-st_sp[c(1,11,12,25,26,27,28,29,30,31,32,33,34,35,38,39,40,41,42,43,44,45,46,47,48,49,50,51)]
-
-View(a)
-
-colnames(st_sp)
-
+names(Financial_Aid)[1] <- "StudentID"
+names(Financial_Aid)[3] <-"cohortterm"
 names(Financial_Aid)[4] <- "MaritalStatus"
+names(Financial_Aid)[5] <- "AdjustedGrossIncome"
 names(Financial_Aid)[6] <- "ParentAdjustedGrossIncome"
 names(Financial_Aid)[7] <- "FatherHighestGradeLevel"
 names(Financial_Aid)[8] <- "MotherHighestGradeLevel"
-names(Financial_Aid)[9] <- "2012Loan"
-names(Financial_Aid)[10] <- "2012Scholarship"
-names(Financial_Aid)[11] <- "2012Work/Study"
-names(Financial_Aid)[11] <- "2012Grant"
-summary(Financial_Aid)
-
-Financial_Aid_1 <- Financial_Aid[c(1,4,5,6,7,8,9,10,11,12,13)]
-
-View(Financial_Aid_1)
-str(Financial_Aid)
-
-Financial_Aid$`Marital Status` <- as.factor(Financial_Aid$`Marital Status`)
+names(Financial_Aid)[10] <- "Loan2012"
+names(Financial_Aid)[11] <- "Scholarship2012"
+names(Financial_Aid)[12] <- "WorkStudy2012"
+names(Financial_Aid)[13] <- "Grant2012"
 
 Financial_Aid$StudentID <- as.numeric(Financial_Aid$StudentID)
+Financial_Aid$cohortterm <- as.factor(Financial_Aid$cohortterm)
+Financial_Aid$MaritalStatus <- as.factor(Financial_Aid$MaritalStatus)
 
-names(Financial_Aid)[1] <- "StudentID"
+#Data cleaning
+library(Hmisc)
 
+table(Financial_Aid$MaritalStatus) #Single =10155 
 
+Financial_Aid$MaritalStatusNew <- impute(Financial_Aid$MaritalStatus,"Single")
+summary(Financial_Aid$MaritalStatusNew)
 
-a_b_c <- merge(Financial_Aid,a,by="StudentID")
+Financial_Aid$AdjustedGrossIncome <- impute(Financial_Aid$AdjustedGrossIncome,0)
+Financial_Aid$ParentAdjustedGrossIncome <- impute(Financial_Aid$ParentAdjustedGrossIncome,0)
+Financial_Aid$FatherHighestGradeLevel <- impute(Financial_Aid$FatherHighestGradeLevel,0)
+Financial_Aid$MotherHighestGradeLevel <- impute(Financial_Aid$MotherHighestGradeLevel,0)
+Financial_Aid$Loan2012 <- impute(Financial_Aid$Loan2012,0)
+Financial_Aid$Scholarship2012 <- impute(Financial_Aid$Scholarship2012,0)
+Financial_Aid$WorkStudy2012 <- impute(Financial_Aid$WorkStudy2012,0)
+Financial_Aid$Grant2012 <- impute(Financial_Aid$Grant2012,0)
 
-View(a_b_c)
+View(Financial_Aid)
 
-nrow(a_b_c)
+colnames(Financial_Aid)
 
-names(a_b_c)
+Financial_Aid_1 <- Financial_Aid[c(1,2,3,34,5,6,7,8,10,11,12,13)]
+View(Financial_Aid_1)
+colSums(is.na(Financial_Aid_1))
 
-str(TrainLabels)
+##Merge static and progress data
+st_sp <- merge(Fall_2011_ST,Fall_2011_SP,by="StudentID")
+View(st_sp)
+colnames(st_sp)
+st_sp <- st_sp[c(-5,-6,-36,-37)]
 
-TrainLabels$StudentID <- as.numeric(TrainLabels$StudentID)
+nrow(st_sp) ##1759
 
-TrainLabels$Dropout <- as.factor(TrainLabels$Dropout)
+##Merge st_sp with financial aid data
 
-withtrain <- merge(a_b_c,TrainLabels,by = "StudentID")
+MergeAll <- merge(st_sp,Financial_Aid_1,by="StudentID")
 
-View(withtrain)
+View(MergeAll)
 
-names(withtrain)
+colnames(MergeAll)
 
+##Merge labelData
 
-Final_data <- withtrain[c(1,)]
+Merge_Label <- merge(MergeAll,TrainLabels,by="StudentID")
+View(Merge_Label)
+
 
 
